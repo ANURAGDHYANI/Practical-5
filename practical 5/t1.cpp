c2b0efa43d44b5e4ed2bdb1471014fef6fdedc20
ANURAG DHYANI <91282002+ANURAGDHYANI@users.noreply.github.com>
2021-10-09 12:31:28 +0530

    Add files via upload

---
+++ practical 5/t1.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	char ch='a';
+	short s=-1;
+	int i=697869;
+	bool b=false;
+	long l=66074321;
+	float f=2.9f;
+	long double ld=67.8956;
+	wchar_t wc=L'a';
+	cout<<"Character variable	:"<<ch<<endl;
+	cout<<"Short variable		:"<<s<<endl;
+	cout<<"Integer variable	:"<<i<<endl;
+	cout<<"Bool variable		:"<<b<<endl;
+	cout<<"Long variable		:"<<l<<endl;
+	cout<<"Float variable		:"<<f<<endl;
+	cout<<"Long Double variable	:"<<ld<<endl;
+	cout<<"Wide Character variable :"<<wc<<endl;
+return 0;
+}
